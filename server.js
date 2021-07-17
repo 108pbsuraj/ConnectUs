f7217389e91656248a5d5fda628a42386fd265e9
iwpvnitsuaj <spsurajkumar.sp@gmail.com>
2021-07-18 04:52:48 +0530

    changes made now

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 // serve static assets in production
 if(process.env.NODE_ENV === 'production'){
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('/app/client/build'));
 
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname + '/app/cliend/build/index.html'));
